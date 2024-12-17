20821533ab3114e448b022098e4564afa4ec0eb5
Tolga BAYRAK <bayraktolga28@gmail.com>
2024-12-17 14:17:15 +0300

    server

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import http from "http";
+import 'dotenv/config';
 
 const app = express();
 const server = http.createServer(app);
