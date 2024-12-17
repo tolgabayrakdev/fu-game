db526d82d28f5e22abcb166860ff7daf453f05c2
Tolga BAYRAK <bayraktolga28@gmail.com>
2024-12-17 14:03:45 +0300

    s

---
+++ client/src/main.tsx
@@ -0,0 +1,17 @@
+import { lazy, Suspense } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router'
+
+const Home = lazy(() => import('./pages/home'));
+
+createRoot(document.getElementById('root')!).render(
+  <Suspense fallback={<div>Loading...</div>}>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </Suspense>
+
+)
---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import http from "http";
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(morgan("dev"));
+app.use(cors({ credentials: true, origin: true }));
+app.use(express.json());
+
+
+server.listen(process.env.PORT || 1234, () => {
+    console.log("Server running on port 1234");
+});
