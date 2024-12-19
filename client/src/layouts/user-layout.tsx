3a264f49110dd2d8716b6d6c616dccac95c1bbf2
Tolga BAYRAK <bayraktolga28@gmail.com>
2024-12-19 10:13:03 +0300

    layout

---
+++ client/src/layouts/user-layout.tsx
@@ -0,0 +1,12 @@
+import AuthProvider from "@/providers/auth-provider";
+import { Outlet } from "react-router";
+
+
+function UserLayout() {
+    return (
+        <section>
+            <Outlet />
+        </section>
+    );
+}
+export default AuthProvider(UserLayout);
